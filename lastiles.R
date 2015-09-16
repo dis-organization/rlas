d03c70a52028e672a2a00863b4df83a5cb1ab230
Michael Sumner <mdsumner@gmail.com>
2015-09-16 23:17:03 +1000

    plot tile

--- lastiles.R
+++ lastiles.R
@@ -3,17 +3,54 @@ fs <- list.files(".", full.names = TRUE, pattern = "las$")
 x <- as.numeric(substr(basename(fs), 1, 3)) * 1000
 y <- as.numeric(substr(basename(fs), 4, 7)) * 1000
 library(graticule)
+library(raster)
+library(rlas)
+library(dismo)
+library(rgdal)
+library(rgl)
+
+library(RTriangle)
+prj <- "+init=EPSG:28355"
 gx <- unique(x)
 gy <- unique(y)
-cx <- gx + diff(gx[1:2])/2
-cy <- gy + diff(gy[1:2])/2
+cx <- x + diff(gx[1:2])/2
+cy <- y + diff(gy[1:2])/2
 
 g <- graticule(c(gx, max(gx) + diff(gx[1:2])), c(gy, max(gy) + diff(gy[1:2])), tiles = TRUE)
-g <- g[extract(g, cbind(head(x, -1) + diff(x[1:2])/2, y = head(y, -1) + diff(y[1:2])/2))$poly.ID, ]
+projection(g) <- prj
+g <- g[match(over(SpatialPoints(cbind(cx, cy), proj4string = CRS(prj)), g)$layer, g$layer), ]
 
-cxy <- as.matrix(expand.grid(cx, cy))
-cxy <- cxy[!is.na(extract(g, cxy)$poly.ID), ]
+g$lasfile <- fs[match(over(SpatialPoints(cbind(cx, cy), proj4string = CRS(prj)), g)$layer, g$layer)]
 plot(g, asp = 1)
-points(cxy)
+points(cx, cy)
+
+im <- gmap(g, type = "satellite")
+plot(im)
+plot(spTransform(g, projection(im)), add = TRUE)
+
+g$lasfile[match(197, g$layer)]
+x <- readLAS(g$lasfile[match(197, g$layer)])
+
+rgl.points(x[,1:3])
+
+im2 <- gmap(g[match(197, g$layer), ], type = "satellite", scale = 2)
+
+plot(im2)
+ex <- drawExtent()
+xx <- cbind(project(project(x[,1:2], prj, inv = TRUE), projection(im2)), x[,3])
+asub <- xx[,1] >= xmin(ex) & xx[,1] <= xmax(ex) & xx[,2] >= ymin(ex) & xx[,2] <= ymax(ex)
+
+xx <- xx[asub, ]
+xx <- xx[!duplicated(xx[,1:2]), ]
+
+xcols <- im2@legend@colortable[extract(im2, xx[,1:2]) + 1]
+
+tri <- RTriangle::triangulate(pslg(P = xx[, 1:2]))
+a <- tetrahedron3d()
+a$vb <- t(cbind(tri$P, xx[,3], 1))
+a$vb[3,a$vb[3,] > 100] <- 0
+a$it <- t(tri$T)
 
 
+plot3d(t(a$vb), col = xcols, asp = "iso", axes = FALSE)
+shade3d(a, col = xcols[a$it])
